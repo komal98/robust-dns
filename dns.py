a03f954aeabbc6a8b5a368c109f9c390544d07e1
kbakshi98 <kbakshi98@gmail.com>
2023-12-09 09:52:14 -0800

    Creating Response: adding transaction id and flags

--- dns.py
+++ dns.py
@@ -6,6 +6,19 @@ ip = '127.0.0.1'
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((ip,port))
 
+def buildresponse(data):
+ 
+ #Transaction ID
+ TransactionID = data[:2]
+ TID = ''
+ for byte in TransactionID:
+   TID += hex(byte)[2:]
+   print(TID)
+
+ # Get the flags
+ Flags = getflags(data[2:4])
+
 while 1:
  data,addr = sock.recvfrom(512)
- print(data)
+ r = buildresponse(data)
+ sock.sendto(r,addr) 
